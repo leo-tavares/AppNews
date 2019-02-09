1b92c7eebe05fbeabc9e83644f933486a160a584
leo-tavares <leonardotavaresdev@gmail.com>
2019-02-09 14:24:44 -0200

    remoção de propriedades desnecessárias

--- src/HomeList.js
+++ src/HomeList.js
@@ -4,19 +4,7 @@ import { View, Image, StyleSheet } from 'react-native';
 export default class HomeList extends React.Component {
 
     static navigationOptions = {
-        title:'App News',
-        tabBarLabel:'Home',
-        tabBarIcon:({focused, tintColor})=> {
-            if(focused){
-                return(
-                    <Image source={require('../assets/images/home_azul.png')} style={styles.icone} />
-                );
-            }else{            
-                return(
-                    <Image source={require('../assets/images/home_preto.png')} style={styles.icone} />
-                );
-            }
-        }
+        title:'App News'
     }
 
     render(){
--- src/HomePosts.js
+++ src/HomePosts.js
@@ -4,19 +4,7 @@ import { View, Image, StyleSheet } from 'react-native';
 export default class HomePosts extends React.Component {
 
     static navigationOptions = {
-        title:'Posts',
-        tabBarLabel:'Home',
-        tabBarIcon:({focused, tintColor})=> {
-            if(focused){
-                return(
-                    <Image source={require('../assets/images/home_azul.png')} style={styles.icone} />
-                );
-            }else{            
-                return(
-                    <Image source={require('../assets/images/home_preto.png')} style={styles.icone} />
-                );
-            }
-        }
+        title:'Posts'
     }
 
     render(){
